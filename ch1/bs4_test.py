a45f3356a50a9377390e44c85cb830d9d642aea1
yewenhe0904 <yewenhe0904@gmail.com>
2017-05-21 09:53:00 -0500

    finish ch1

--- ch1/bs4_test.py
+++ ch1/bs4_test.py
@@ -1,5 +1,31 @@
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj=BeautifulSoup(html.read(),"lxml")
-print(bsObj.h1)
+from urllib.error import HTTPError,URLError
+from bs4 import BeautifulSoup
+import sys
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except (HTTPError, URLError) as e:
+        print(e)
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(),"lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+# Wrong URL
+title = getTitle("http://www.pythonsscraping.com/exercises/exercise1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
+# Good URL
+title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
